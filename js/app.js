cbcb970c312cc7f67e642290c9bef15cbb355d5a
azaelsanchez <cem20619@gmail.com>
2019-11-08 08:11:54 +0100

    app al 90%
    Falta la maquetacion y una funcion de parar la musica.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
+    let nombres = [];
     SC.initialize({
-        client_id:'aa06b0630e34d6055f9c6f8beb8e02eb'
+        client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
     });
-})
+    //Creamos un keypress para el buscador.
+    $(document).bind('keypress', function(teclado) {
+        if(teclado.keyCode==13){ $('#searchSongs').trigger('click');}
+    });
+    //Creamos la funcion de busqueda para capturar lo que queremos.
+    $("#searchSongs").click(function() {
+        $("#songList").children().remove();
+        let songQ = $("#querySong").val();
+        SC.get('/tracks', {
+            q: songQ
+        })
+        .then(function(tracks) {
+            nombres = tracks;
+            console.log(tracks);
+            nombres.forEach( function(data) {
+                let player = data.artwork_url;
+                if(data.artwork_url === null) player = data.user.avatar_url;
+                let persona = '<div class="col5 mb-3 mt-3 d-flex justify-content-center"><img title="'+ data.title +'" id="'+ data.id +'"ondragstart="drag(event)" draggable="true" src="'+ player + '"></img></div>';
+                $("#resultado").append(persona);
+            })
+        });
+    });
+ });
+
+ //Realizamos el Drag y el Drop.
+
+ function drag(ev){
+    ev.dataTransfer.setData("text",ev.target.id);
+ }
+ function allowDrop(ev){
+    ev.preventDefault();
+ }
+ function drop(ev){
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    //console.log(document.getElementById(data));
+    ev.target.value = document.getElementById(data).title;
+    let query = data;
+    $('#sound').toggleClass('pulse');
+    SC.stream('/tracks/'+ query +'').then(function(player){
+        player.play().then(function(){
+            console.log('reproduciendo');
+        }).catch(function(e){
+            console.error('no funciona.', e);
+        });
+    });
+ }
